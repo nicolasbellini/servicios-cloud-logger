36909710ad24fc8d5e100037f944c9a38fed1c23
Nicolas Bellini <nicolasgbellini@gmail.com>
2021-07-19 19:50:07 -0300

    escribir logs local

--- src/routes/logger.route.js
+++ src/routes/logger.route.js
@@ -2,9 +2,8 @@ import express from "express";
 
 import BadParamError from "../exceptions/badParamError";
 import { existParams } from "../utils/utils";
-import { Loggly } from "winston-loggly-bulk";
-import winston from "winston";
-import config from "../config/config";
+import {logger} from "../config/winston";
+
 
 export const loggerRouter = express.Router();
 
@@ -14,14 +13,8 @@ loggerRouter.post('/log', async (req, res, next) => {
     if(!existParams(validParams, req.body)) throw new BadParamError(validParams);
     const level = req.body.level;
     const message = req.body.message;
+    logger.log(level, message)
 
-    winston.add(new Loggly({
-      token: config.TOKEN,
-      subdomain: config.SUBDOMAIN,
-      tags: ["Winston-NodeJS"],
-      json: true
-    }));
-    winston.log(level, message);
 
     res.status(200).json({});
     next();
